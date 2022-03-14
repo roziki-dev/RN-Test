726b71029dc84aa4744096f8d9112094a8dffc48
rOziy <roziyexplor3@gmail.com>
2022-03-14 21:00:40 +0700

    style for global view

--- src/styles/themeDefault.js
+++ src/styles/themeDefault.js
@@ -1,8 +1,11 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
+import { colors } from './colors'
+
+export const screen = Dimensions.get('window')
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: colors.white
   }
 })
