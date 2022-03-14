0638c0fe439407c02ccc1bcf4759e688a8a9f605
rOziy <roziyexplor3@gmail.com>
2022-03-14 21:00:23 +0700

    set default color

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#fdd835',
+  secondary: '#00a680',
+  textPrimary: '#00a680',
+  textDisabled: '#898987',
+  textSecondary: '#00a680',
+  white: '#ffffff',
+  gray: '#d7ccc8',
+  black: '#000000',
+  statusBar: '#FFC300'
+}
