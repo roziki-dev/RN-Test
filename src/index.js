21e60c88ee4add834363a1068e8b55cabfd9117b
rOziy <roziyexplor3@gmail.com>
2022-03-15 01:54:26 +0700

    redux provide

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import RootNavigation from './navigation/root'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import reducers from './store/reducer'
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 const App = () => {
   return (
-    <RootNavigation />
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
   )
 }
 
