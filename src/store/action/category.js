6f3e3621190a416436c98213236826d6d157b37b
rOziy <roziyexplor3@gmail.com>
2022-03-15 16:24:30 +0700

    update category action

--- src/store/action/category.js
+++ src/store/action/category.js
@@ -1,6 +1,5 @@
 import request from '../../lib/axios'
 import { FETCH_CATEGORIES, FETCH_CATEGORIES_SUCCESS, FETCH_CATEGORIES_FAIL } from '../types'
-// import {  } from "module";
 
 const defaultParams = {
   category: '',
@@ -13,24 +12,38 @@ const defaultParams = {
 export const getCategory = (data = { ...defaultParams }) => {
   return (dispatch, state) => {
     dispatch(Category())
-    console.log(data.category)
     request({
-      url: `/${data.category}/`,
+      url: `${data.category}/`,
       method: 'GET'
     }).then((rsp) => {
       const arr = []
-      console.log(rsp.data.count)
-      if (rsp.data.results.length > 0) {
-        for (let index = 0; index < rsp.data.results.length; index++) {
-          arr.push({
-            ...rsp.data.results[index],
-            imgPath: `asset:/${data.category}/${index + 1}.jpg`
-          })
+      // console.log(rsp)
+      if (rsp.results.length > 0) {
+        let indx = 0
+        for (const item of rsp.results) {
+          let url = ''
+          let str = ''
+          switch (data.category) {
+            case 'starships':
+              str = item.url
+              str = str.split('/')
+              // console.log(str[str.length - 2])
+              url = `asset:/${data.category}/${str[str.length - 2]}.jpg`
+              break
+            default:
+              url = `asset:/${data.category}/${indx + 1}.jpg`
+              break
+          }
+          const obj = {
+            ...item,
+            imgPath: url
+          }
+          arr.push(obj)
+          indx++
         }
       }
-      dispatch(CategorySuccess({ ...rsp.data, results: arr }))
+      dispatch(CategorySuccess({ ...rsp, results: arr }))
     }).catch((err) => {
-      console.log(err)
       dispatch(CategoryFail(err))
     })
   }
