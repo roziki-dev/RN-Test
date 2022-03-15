9a3eeb14f2319f0eb5441c46952b08e0a431287b
rOziy <roziyexplor3@gmail.com>
2022-03-15 16:24:43 +0700

    axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,9 +3,25 @@ import { baseUrl } from '../config/baseUrl'
 
 const timeout = 1200
 
-const request = axios.create({
+const client = axios.create({
   baseURL: baseUrl,
   timeout: timeout
 })
+
+const request = async function (options) {
+  const onSuccess = function (response) {
+    return response.data
+  }
+  const onError = function (error) {
+    return Promise.reject(error.response || error.message || error.response.status || error.response.data)
+  }
+
+  try {
+    const response = await client(options)
+    return onSuccess(response)
+  } catch (error) {
+    return onError(error)
+  }
+}
 
 export default request
